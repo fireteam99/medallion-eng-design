5237c11a6073acf9d36fbd48efb4ed9c78a06822
fireteam99 <raymancheungsy@gmail.com>
2022-03-21 03:08:47 -0400

    finished ui stuff

---
+++ pages/doctors/[doctor].js
@@ -0,0 +1,109 @@
+import {
+  Container,
+  HStack,
+  VStack,
+  Text,
+  Spinner,
+  Alert,
+  Heading,
+  Button,
+  Link,
+} from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import useSWR from "swr";
+import fetcher from "utils/fetcher";
+import NextLink from "next/link";
+import { PlusSquareIcon, ChevronLeftIcon } from "@chakra-ui/icons";
+
+export default function Doctor() {
+  const router = useRouter();
+  const { doctor: doctorId } = router.query;
+
+  const { data: doctorsData, error: doctorsError } = useSWR(
+    "/api/doctors",
+    fetcher
+  );
+  console.log(doctorsError);
+  const doctorsIsLoading = !doctorsError && !doctorsData;
+
+  const doctor = doctorsData?.doctors?.find((doctor) => doctor.id === doctorId);
+
+  const { data: formsData, error: formsError } = useSWR("/api/forms", fetcher);
+  const formsIsLoading = !formsError && !formsData;
+
+  return (
+    <Container>
+      <VStack mt="1em" spacing="1em">
+        <Heading>Doctor Information</Heading>
+        {doctorsIsLoading && <Spinner />}
+        {doctorsError && (
+          <Alert>Something went wrong loading doctor information.</Alert>
+        )}
+        {doctor && (
+          <VStack
+            p="1em"
+            borderRadius="md"
+            shadow="md"
+            align="start"
+            bg="gray.50"
+          >
+            <DataField title="First Name:" value={doctor.firstName} />
+            <DataField title="Last Name:" value={doctor.lastName} />
+            <DataField title="Gender:" value={doctor.gender} />
+            <DataField title="Date of Birth:" value={doctor.birthday} />
+          </VStack>
+        )}
+        {formsIsLoading && <Spinner />}
+        {formsError && (
+          <Alert>Something went wrong loading form information.</Alert>
+        )}
+        {formsData && (
+          <VStack>
+            {formsData.forms.map((form) => (
+              <HStack
+                key={form.id}
+                borderRadius="md"
+                shadow="md"
+                p="1em"
+                bg="cyan.100"
+              >
+                <Link href={form.url} target="_blank" rel="noreferrer">
+                  {form.name}
+                </Link>
+                <NextLink
+                  href={`/api/doctors/${doctorId}/forms/${form.id}`}
+                  passHref
+                >
+                  <a target="_blank" rel="noreferrer">
+                    <Button variant="ghost" colorScheme="black">
+                      <HStack>
+                        <Text>Fill</Text> <PlusSquareIcon />
+                      </HStack>
+                    </Button>
+                  </a>
+                </NextLink>
+              </HStack>
+            ))}
+          </VStack>
+        )}
+        <NextLink href="/" passHref>
+          <Link>
+            <HStack spacing={1}>
+              <ChevronLeftIcon />
+              <Text>Back</Text>
+            </HStack>
+          </Link>
+        </NextLink>
+      </VStack>
+    </Container>
+  );
+}
+
+function DataField({ title, value }) {
+  return (
+    <HStack>
+      <Heading size="sm">{title}</Heading>
+      <Text>{value}</Text>
+    </HStack>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,60 @@
+import {
+  Container,
+  Heading,
+  Center,
+  Spinner,
+  Alert,
+  VStack,
+  Text,
+  HStack,
+  Box,
+} from "@chakra-ui/react";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
+import useSWR from "swr";
+import Link from "next/link";
+
+import fetcher from "utils/fetcher";
+
 export default function Home() {
-  return <div>Home</div>;
+  const { data, error } = useSWR("/api/doctors", fetcher);
+  const isLoading = !error && !data;
+  return (
+    <Container>
+      <Center mt="2em">
+        <VStack spacing="1em">
+          <VStack>
+            <Heading>Form Filler</Heading>
+            <Text>Select a doctor to view available forms.</Text>
+          </VStack>
+          {isLoading && <Spinner />}
+          {error && <Alert status="error">Unexpected Error</Alert>}
+          {data && (
+            <VStack>
+              {data.doctors.map((doctor) => (
+                <Link href={`/doctors/${doctor.id}`} key={doctor.id} passHref>
+                  <HStack
+                    bg="cyan.100"
+                    p=".8em"
+                    borderRadius="md"
+                    shadow="md"
+                    width="sm"
+                    justifyContent="space-between"
+                    cursor="pointer"
+                  >
+                    <VStack align="start">
+                      <Heading size="md">
+                        {doctor.firstName} {doctor.lastName}
+                      </Heading>
+                      <Text fontSize="sm">{doctor.birthday}</Text>
+                    </VStack>
+                    <ArrowForwardIcon w={8} h={8} />
+                  </HStack>
+                </Link>
+              ))}{" "}
+            </VStack>
+          )}
+        </VStack>
+      </Center>
+    </Container>
+  );
 }
